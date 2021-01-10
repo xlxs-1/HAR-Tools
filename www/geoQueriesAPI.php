36b94117ad79d1e2c434496b4a2a02826a022a67
xlxs <spyrosr@hotmail.com>
2021-01-11 00:08:41 +0200

    a

--- www/geoQueriesAPI.php
+++ www/geoQueriesAPI.php
@@ -2,20 +2,13 @@
 if (isset($_SESSION["email"])) {
   include ($_SERVER["DOCUMENT_ROOT"].'/../utils.php');
   $database=new database();
-  //var_dump(123);
+  $user=$database->getAccount_($_SESSION["email"]);
   
-  //$subdivisions=12;
-  
-  // $points = {
-  //   "data": [{lat: Math.random(), lng:Math.random(), count: 3}]
-  // };
-
-  // echo($points);
-
-
-
-  $arr=$database->getHttp_requestsAsJsonPoints($_SESSION["email"]);
-  //$jArr=json_encode('{'.$arr.'}');
+  if ($user["is_admin"]) {
+    $arr=$database->getHttp_requestsAsJsonPoints_();
+  }else{
+    $arr=$database->getHttp_requestsAsJsonPoints($_SESSION["email"]);
+  }
   echo($arr);
 }
 ?>
